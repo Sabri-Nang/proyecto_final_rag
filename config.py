7527c811034df420fbe56e82385d3e999eb21577
Sabrina Sanches <sabrinadelosangelessanches@gmail.com>
2026-06-23 11:33:23 -0300

    Agrego la variable MODO_LOCAL

--- config.py
+++ config.py
@@ -1,17 +1,33 @@
 import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# ==============================================================================
+# 🎛️ CONFIGURACIÓN PRINCIPAL: ¿Estás en tu computadora o en Hugging Face?
+# ==============================================================================
+MODO_LOCAL = True  # Poner en False para subir a Hugging Face Spaces 🚀
+# ==============================================================================
 
 # Autenticación y API de HuggingFace
 HF_TOKEN = os.environ.get("HF_TOKEN", "")
-MODEL_ID = "meta-llama/Llama-3.2-3B-Instruct"
 
-if not HF_TOKEN:
-    raise ValueError("Configurá el secreto HF_TOKEN en el Space.")
+# Configuración según el entorno
+if MODO_LOCAL:
+    MODEL_NAME = "gemma4:e2b"
+    NUM_CTX = 2048
+else:
+    # PArámetros para Hugging Face
+    MODEL_ID = "Qwen/Qwen2.5-Coder-7B-Instruct"
+    if not HF_TOKEN:
+        raise ValueError("Configurá el secreto HF_TOKEN en el Space.")
 
 # Configuración de Embeddings y Base Vectorial
 EMBEDDING_MODEL = "intfloat/multilingual-e5-large"
 COLLECTION_NAME = "proyecto_rag_spaces"
 
 # Parámetros del Divisor de Texto
-CHUNK_SIZE = 600
-CHUNK_OVERLAP = 80
+CHUNK_SIZE = 1000
+CHUNK_OVERLAP = 200
 SEPARATORS = ["\n\n", "\n", ". ", " "]
